fc318cb45af5719b4da0534c4fe4b2cc3cbe3624
gamoridev <gamoridev@protonmail.com>
2017-11-26 13:37:36 -0200

    Remoção de funções inúteis e adição de comentários

--- Sensor_Umidade_Rele/Sensor_Umidade_Rele.ino
+++ Sensor_Umidade_Rele/Sensor_Umidade_Rele.ino
@@ -1,30 +1,31 @@
-#include <NilRTOS.h>
+#include <NilRTOS.h>  // Inclusão da biblioteca NilRTOS
 
-const uint8_t sinal = A5; // Sinal analógico do sensor de umidade está settado no pino A0 do Arduino
+const uint8_t sinal = A5; // Sinal analógico do sensor de umidade está settado no pino A5 do Arduino
 const uint8_t led = 13; // Led está no pino 13
 const uint8_t relay = A0;  // Sinal analógico do relé
 
-SEMAPHORE_DECL(sem, 0);
+SEMAPHORE_DECL(sem, 0); // Declaração do semáforo para uso de threads
 
-NIL_WORKING_AREA(waThread1, 128);
+NIL_WORKING_AREA(waThread1, 128);  // Definição do workspace da thread 1
 
+//  Thread 1: aguarda sinal da thread 2 e dispara sinal ao relé e ao led, ativando o sistema de irrigação
 NIL_THREAD(Thread1, arg) {  
   while (TRUE) {
     // Espera sinal da thread 2.
     nilSemWait(&sem);
 
-    digitalWrite(led, HIGH);
-    digitalWrite(relay, LOW);
+    digitalWrite(led, HIGH); // Liga o LED
+    digitalWrite(relay, LOW); // Liga o Relé
     nilThdSleepMilliseconds(500);
   }
 }
 
-NIL_WORKING_AREA(waThread2, 128);
+NIL_WORKING_AREA(waThread2, 128); // Workspace da thread 2
 
-// Declare the thread function for thread 2.
+// Thread 2: envia sinal para thread 1 e lê os valores analógicos dos sensores e atuadores
 NIL_THREAD(Thread2, arg) {
   while (TRUE) {
-    pinMode(sinal, INPUT);
+    pinMode(sinal, INPUT);  // Recupera a entrada do sensor de umidade
     pinMode(relay, OUTPUT);
     pinMode(led, OUTPUT);
   
@@ -32,19 +33,19 @@ NIL_THREAD(Thread2, arg) {
     int umidade;
     boolean liga;
 
-    valorSinal = analogRead(sinal);
-    liga = verificaValorUmidade(valorSinal);
+    valorSinal = analogRead(sinal);  // lê sinal analógico e guarda na variável iniciada anterioremente
+    liga = verificaValorUmidade(valorSinal);  // envia valor para a função que calcula e envia resposta de acordo com o valor do resultado
     if (liga) {
-      digitalWrite(led, LOW);
+      digitalWrite(led, LOW);  // condição como true, o LED continua desligado assim como o relé
       digitalWrite(relay, HIGH);
     } else {
-      nilSemSignal(&sem);
+      nilSemSignal(&sem); // condição como false, envia sinal para tread 2, ativando o sistema de irrigação
     }
-    //printaResultados(valorSinal, porcentagem);
     nilThdSleepMilliseconds(1000);
   }
 }
 
+// Iniciação das threads e workspaces
 NIL_THREADS_TABLE_BEGIN()
 NIL_THREADS_TABLE_ENTRY("thread1", Thread1, NULL, waThread1, sizeof(waThread1))
 NIL_THREADS_TABLE_ENTRY("thread2", Thread2, NULL, waThread2, sizeof(waThread2))
@@ -52,7 +53,7 @@ NIL_THREADS_TABLE_END()
 
 void setup()
 {
-  // Start Nil RTOS.
+  // Setup necessário para as threads e biblioteca do nilrtos
   Serial.begin(9600);
   nilSysBegin();
 }
@@ -80,13 +81,3 @@ boolean verificaValorUmidade(int valor) {
     return false;
   }
 }
-
-// Mostra os resultados no log
-void printaResultados(int valorSinal, int porcentagem){
-  Serial.print("\n\nValor analógico: ");
-  Serial.print(valorSinal);
-  Serial.print("\nPorcentagem: "+ porcentagem);
-  Serial.print(porcentagem);
-  Serial.print("%");
-}
-
